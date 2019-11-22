d8b030b7c99febd987306802bbcd9b1d9733789a
Filip Jerga <jerga99@gmail.com>
2019-11-22 11:02:52 +0100

    5 Class Component State

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,8 +5,12 @@ import React from 'react'
 import './Home.css'
 
 
-// const Home = () => {
+// 1. Smaller components
+// 2. Usualy presentational component
+// 3. In most of cases reusable components
+// 4. Focus on one responsibility
 
+// const Home = () => {
   // return (
   //   <div className="container">
   //     <h1>I am Home Page</h1>
@@ -14,16 +18,39 @@ import './Home.css'
   // )
 // }
 
-
+// 1. Larger components, container components
+// 2. Easier to handle lots of state
+// 3. More boilerplate
+// 4. Access to lifecycle functions
 
 class Home extends React.Component {
 
+  constructor(props) {
+    super(props)
+    
+    this.state = {
+      message: 'Super Message!!!!'
+    }
+  }
 
+  // state = {
+  //   message: 'Super Message!!!!'
+  // }
 
+  // lifecycle function
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({message: 'I am updated Message!!!!!!'})
+    }, 1000)
+  }
+
+  // lifecycle function
   render() {
+    const { message } = this.state
     return (
       <div className="container">
         <h1>I am Home Class Page</h1>
+        <p>{message}</p>
       </div>
     )
   }
