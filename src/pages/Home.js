04295f688570345237e3255bc2e3497edd65576f
Filip Jerga <jerga99@gmail.com>
2019-11-22 15:38:50 +0100

    8 Counter in Class Component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,75 +5,84 @@ import React, { useState, useEffect } from 'react'
 import './Home.css'
 
 
-const Home = () => {
+// const Home = () => {
 
-  const [message, setMessage] = useState('Super Message!!!')
-  const [count, setCount] = useState(0)
-  // const [test, setTest] = useState(0)
+//   const [message, setMessage] = useState('Super Message!!!')
+//   const [count, setCount] = useState(0)
+//   // const [test, setTest] = useState(0)
 
-  useEffect(() => {
-    setTimeout(() => {
-      setMessage('I am updated Message!!!!!!')
-    }, 1000)
-  }, [])
-
-  const increment = () => {
-    // setTest(test + 1)
-    setCount(count + 1)
-  }
-
-  const decrement = () => {
-    setCount(count - 1)
-  }
-
-  return (
-    <div className="container">
-      <h1>I am Home Page</h1>
-      <p>{message}</p>
-      <button onClick={increment}>Increment</button>
-       <div className="counter"> {count} </div>
-      <button onClick={decrement}>Decrement</button>
-    </div>
-  )
-}
-
-// 1. Larger components, container components
-// 2. Easier to handle lots of state
-// 3. More boilerplate
-// 4. Access to lifecycle functions
-
-// class Home extends React.Component {
-
-//   constructor(props) {
-//     super(props)
-
-//     this.state = {
-//       message: 'Super Message!!!!'
-//     }
-//   }
-
-//   // state = {
-//   //   message: 'Super Message!!!!'
-//   // }
-
-//   // lifecycle function
-//   componentDidMount() {
+//   useEffect(() => {
 //     setTimeout(() => {
-//       this.setState({message: 'I am updated Message!!!!!!'})
+//       setMessage('I am updated Message!!!!!!')
 //     }, 1000)
+//   }, [])
+
+//   const increment = () => {
+//     // setTest(test + 1)
+//     setCount(count + 1)
 //   }
 
-//   // lifecycle function
-//   render() {
-//     const { message } = this.state
-//     return (
-//       <div className="container">
-//         <h1>I am Home Class Page</h1>
-//         <p>{message}</p>
-//       </div>
-//     )
+//   const decrement = () => {
+//     setCount(count - 1)
 //   }
+
+//   return (
+//     <div className="container">
+//       <h1>I am Home Page</h1>
+//       <p>{message}</p>
+//       <button onClick={increment}>Increment</button>
+//        <div className="counter"> {count} </div>
+//       <button onClick={decrement}>Decrement</button>
+//     </div>
+//   )
 // }
+
+
+class Home extends React.Component {
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      message: 'Super Message!!!!',
+      count: 0
+    }
+
+    this.decrement = this.decrement.bind(this)
+  }
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({message: 'I am updated Message!!!!!!'})
+    }, 1000)
+  }
+
+  increment = () => {
+    const { count } = this.state
+    this.setState({count: count + 1})
+  }
+
+  decrement() {
+    const { count } = this.state
+    this.setState({count: count - 1})
+  }
+
+  // lifecycle function
+  render() {
+    const { message, count } = this.state
+    return (
+      <div className="container">
+        <h1>I am Home Page</h1>
+        <p>{message}</p>
+        <button onClick={this.increment}>Increment</button>
+         <div className="counter"> {count} </div>
+        { /* <button onClick={() => this.decrement()}>Decrement</button> */ }
+        { /* <button onClick={this.decrement.bind(this)}>Decrement</button> */ }
+         <button onClick={this.decrement}>Decrement</button>
+      </div>
+    )
+  }
+}
 
 
 
