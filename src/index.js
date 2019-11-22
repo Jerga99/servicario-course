a1d7b65596273e9adab9d8fdfd82fee9b7f61f7c
Filip Jerga <jerga99@gmail.com>
2019-11-22 09:50:25 +0100

    3 Functional Home Component

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './App'; // Our App Functional Component
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
---
+++ src/pages/Home.js
@@ -0,0 +1,17 @@
+
+
+
+import React from 'react'
+import './Home.css'
+
+
+const Home = () => {
+
+  return (
+    <div className="container">
+      <h1>I am Home Page</h1>
+    </div>
+  )
+}
+
+export default Home
