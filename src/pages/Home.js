550ddba560483bb2e332949b41ea5ff0e4df12a7
Filip Jerga <jerga99@gmail.com>
2019-11-22 10:27:26 +0100

    4 Class vs Functional Components

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,13 +5,35 @@ import React from 'react'
 import './Home.css'
 
 
-const Home = () => {
+// const Home = () => {
 
-  return (
-    <div className="container">
-      <h1>I am Home Page</h1>
-    </div>
-  )
+  // return (
+  //   <div className="container">
+  //     <h1>I am Home Page</h1>
+  //   </div>
+  // )
+// }
+
+
+
+class Home extends React.Component {
+
+
+
+  render() {
+    return (
+      <div className="container">
+        <h1>I am Home Class Page</h1>
+      </div>
+    )
+  }
 }
 
+
+
+
+
 export default Home
+
+
+
