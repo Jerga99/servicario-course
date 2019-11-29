d1a49fc35653f0bc7cd82198cabad3acd30b349b
Filip Jerga <jerga99@gmail.com>
2019-11-29 10:25:49 +0100

    55 Handles Form Submit

--- src/pages/services/ServiceCreate.js
+++ src/pages/services/ServiceCreate.js
@@ -1,8 +1,25 @@
 
-import React from 'react'
+import React, { useState } from 'react'
 
 
 const ServiceCreate = () => {
+
+  const [ serviceForm, setServiceForm ] = useState({
+    category: 'mathematics',
+    title: '',
+    description: '',
+    image: '',
+    price: null
+  })
+
+  const handleChange = e => {
+    const { name, value } = e.target
+    setServiceForm({...serviceForm, [name]: value})
+  }
+
+  const handleSubmit = () => {
+    alert(JSON.stringify(serviceForm))
+  }
 
   return (
     <div className="create-page">
@@ -14,9 +31,12 @@ const ServiceCreate = () => {
               <label className="label">Category</label>
               <div className="control">
                 <div className="select">
-                  <select>
-                    <option value="service">Service</option>
-                    <option value="product">Product</option>
+                  <select name="category" onChange={handleChange}>
+                    <option value="mathematics">Mathematics</option>
+                    <option value="programming">Programming</option>
+                    <option value="painting">Painting</option>
+                    <option value="singing">Singing</option>
+                    <option value="english">English</option>
                   </select>
                 </div>
               </div>
@@ -25,6 +45,8 @@ const ServiceCreate = () => {
               <label className="label">Title</label>
               <div className="control">
                 <input
+                  onChange={handleChange}
+                  name="title"
                   className="input"
                   type="text"
                   placeholder="Text input" />
@@ -34,7 +56,8 @@ const ServiceCreate = () => {
               <label className="label">Description</label>
               <div className="control">
                 <textarea
-                  v-model="form.description"
+                  onChange={handleChange}
+                  name="description"
                   className="textarea"
                   placeholder="Textarea"></textarea>
               </div>
@@ -43,6 +66,8 @@ const ServiceCreate = () => {
               <label className="label">Image Url</label>
               <div className="control">
                 <input
+                  onChange={handleChange}
+                  name="image"
                   className="input"
                   type="text"
                   placeholder="Text input" />
@@ -52,15 +77,19 @@ const ServiceCreate = () => {
               <label className="label">Price per Hour</label>
               <div className="control">
                 <input
+                  onChange={handleChange}
+                  name="price"
                   className="input"
-                  type="text"
+                  type="number"
                   placeholder="Text input" />
               </div>
             </div>
             <div className="field is-grouped">
               <div className="control">
                 <button
-                  type="button" className="button is-link">Create</button>
+                  onClick={handleSubmit}
+                  type="button" 
+                  className="button is-link">Create</button>
               </div>
               <div className="control">
                 <button className="button is-text">Cancel</button>
